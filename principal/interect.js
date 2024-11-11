6c20a1bdcb920a6f664e4c267d4f28e843dfb56d
Gravazinhun <nuvemgravazinhun@gmail.com>
2024-11-11 16:18:55 -0300

    Add files via upload

---
+++ principal/interect.js
@@ -0,0 +1,14 @@
+function showDetails(protocol) {
+    // Esconde todos os detalhes
+    let details = document.querySelectorAll('.protocol-details');
+    details.forEach(function(detail) {
+        detail.style.display = 'none';
+    });
+
+    // Exibe os detalhes do protocolo selecionado
+    let selectedDetail = document.getElementById(protocol);
+    if (selectedDetail) {
+        selectedDetail.style.display = 'block';
+    
+    }
+}
