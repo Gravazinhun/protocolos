cfda2a4b3c648a6e8a84b53eb6ac96921babc766
Alexxz135 <alexandre.batista2@aluno.ce.gov.br>
2024-11-13 09:45:43 -0300

    Update interect.js

--- principal/interect.js
+++ principal/interect.js
@@ -1,14 +1,14 @@
-function showDetails(protocol) {
-    // Esconde todos os detalhes
-    let details = document.querySelectorAll('.protocol-details');
-    details.forEach(function(detail) {
-        detail.style.display = 'none';
-    });
+function showDetails(protocolId){
+    const allDetails = document.querySelectorAll('.protocol-details');
 
-    // Exibe os detalhes do protocolo selecionado
-    let selectedDetail = document.getElementById(protocol);
-    if (selectedDetail) {
-        selectedDetail.style.display = 'block';
+    allDetails.forEach(detail => {
+        if (detail.id !== protocolId) {
+            detail.classList.remove('visible');
+        }
+    });
     
+    const selectedElement = document.getElementById(protocolId);
+    if(selectedElement) {
+        selectedElement.classList.toggle('visible');
     }
 }
